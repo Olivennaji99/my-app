451e4dd93aa27ecce1b0e177f2357002e567174e
OLIVE NNAJI <olivennaji99@gmail.com>
2026-02-12 15:41:37 +0100

    message

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+export default  function Navbar()  {
+    return(
+        <>
+        <nav className="navbar">
+            <div className="container">
+                <a href="#" className="logo">My 1st React App</a>
+
+
+                <ul className="nav-links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Service</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+        </>
+    )
+
+    
+}
